07b2cfcaf15e739b7bb8aaa8620e885b04b66af8
Nick Morhun <nickmorgunn@gmail.com>
2019-12-17 11:28:14 +0200

    Improved a unit test

--- Assets/NickMorhun/UnityHelpers/PlayModeTests/GameObjectNameTests.cs
+++ Assets/NickMorhun/UnityHelpers/PlayModeTests/GameObjectNameTests.cs
@@ -9,14 +9,25 @@ namespace Tests
         [Test]
         public void GetFullName_ReturnsHierarchyNames()
         {
-            var testObject = new GameObject("root object");
-            var testObject1 = new GameObject(" ");
-            var testObject2 = new GameObject("test object");
+            var rootObject = new GameObject("root object");
 
-            testObject1.transform.parent = testObject.transform;
-            testObject2.transform.parent = testObject1.transform;
+            var level1Object1 = new GameObject("level1 Object1");
+            var level1Object2 = new GameObject("level1 Object2");
+            var level1Object3 = new GameObject("level1 Object3");
 
-            Assert.AreEqual("root object/ /test object", testObject2.GetFullName());
+            var level2Object1 = new GameObject("level2 Object1");
+            var level2Object2 = new GameObject("level2 Object2");
+            var level2Object3 = new GameObject("level2 Object3");
+
+            level1Object1.transform.parent = rootObject.transform;
+            level1Object2.transform.parent = rootObject.transform;
+            level1Object3.transform.parent = rootObject.transform;
+
+            level2Object1.transform.parent = level1Object1.transform;
+            level2Object2.transform.parent = level1Object2.transform;
+            level2Object3.transform.parent = level1Object2.transform;
+
+            Assert.AreEqual("root object/level1 Object2/level2 Object3", level2Object3.GetFullName());
         }   
     }
 }
