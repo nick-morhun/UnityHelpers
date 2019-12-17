2fc1b60bbc09070f2ca5908fd1c53ca4b3c77247
Nick Morhun <nickmorgunn@gmail.com>
2019-12-17 11:22:50 +0200

    Added unit tests

---
+++ Assets/NickMorhun/UnityHelpers/PlayModeTests/GameObjectNameTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityHelpers.Runtime;
+
+namespace Tests
+{
+    public class GameObjectNameTests
+    {
+        [Test]
+        public void GetFullName_ReturnsHierarchyNames()
+        {
+            var testObject = new GameObject("root object");
+            var testObject1 = new GameObject(" ");
+            var testObject2 = new GameObject("test object");
+
+            testObject1.transform.parent = testObject.transform;
+            testObject2.transform.parent = testObject1.transform;
+
+            Assert.AreEqual("root object/ /test object", testObject2.GetFullName());
+        }   
+    }
+}
---
+++ Assets/NickMorhun/UnityHelpers/PlayModeTests/UnityComponentDestructionTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityHelpers.Runtime;
+
+namespace Tests
+{
+    public class UnityComponentDestructionTests
+    {
+        private const float TestsTimeScale = 10f;
+
+        private static readonly float[] delays = new float[] { 0f, .01f, 2f };
+
+        [Test]
+        public void DestroyObjectImmediate_DestroysGameObject()
+        {
+            var testObject = new GameObject("test object");
+
+            testObject.transform.DestroyObjectImmediate();
+
+            Assert.IsFalse(testObject);
+        }
+
+        [UnityTest]
+        public IEnumerator DestroyObject_DestroysGameObject()
+        {
+            var testObject = new GameObject("test object");
+
+            testObject.transform.DestroyObject();
+
+            yield return null;
+
+            Assert.IsFalse(testObject);
+        }
+
+        [UnityTest]
+        public IEnumerator DestroyObject_WithDelay_DestroysGameObject([ValueSource(nameof(delays))] float delay)
+        {
+            float coroutineDelay = Mathf.Max(delay * 1.1f, .01f);
+            Time.timeScale = TestsTimeScale;
+            var testObject = new GameObject("test object");
+
+            testObject.transform.DestroyObject(delay);
+
+            yield return new WaitForSeconds(coroutineDelay);
+
+            Time.timeScale = 1f;
+            Assert.IsFalse(testObject);
+        }
+    }
+}
